4871b215793eea48fcae23f6dea163103439b760
neogiciel <123723616+neogiciel@users.noreply.github.com>
2024-01-02 16:19:19 +0100

    version 1.12

--- src/test/java/com/neogiciel/HomeControllerTest.java
+++ src/test/java/com/neogiciel/HomeControllerTest.java
@@ -11,7 +11,7 @@ class HomeControllerTest {
     @Test
     void testHelloEndpoint() {
         given()
-          .when().get("/hello")
+          .when().get("/test")
           .then()
              .statusCode(200)
              .body(is("Hello from RESTEasy Reactive"));
