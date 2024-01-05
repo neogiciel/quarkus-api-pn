5e92ebd2fa3955a1fb02dba6b14c54f5b3f33e9a
neogiciel <123723616+neogiciel@users.noreply.github.com>
2024-01-05 16:52:03 +0100

    sources v1.2

--- src/main/java/com/neogiciel/ApiController.java
+++ src/main/java/com/neogiciel/ApiController.java
@@ -1,6 +1,7 @@
 package com.neogiciel;
 
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -21,7 +22,9 @@ import com.neogiciel.model.Personne;
 @Path("/")
 public class ApiController {
 
-    
+    @ConfigProperty(name = "application.version") 
+    String applicationVersion;
+
     //Metriques
     @Inject
     MeterRegistry registry;
@@ -29,17 +32,24 @@ public class ApiController {
     //Peronnse
     PersonneService personneService = new PersonneService();
 
+    /*
+     * api
+     */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/api")
     public String api() {
+
+        registry.counter("api", Tags.of("name", "api")).increment();
         
         JSONObject obj = new JSONObject();
         obj.put("api", "api quarkus");
         return obj.toString();
     }
 
-
+    /*
+     * liste 
+     */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/api/liste")
@@ -52,25 +62,47 @@ public class ApiController {
         return jsonArray.toString();
      }
 
+    /*
+     * get 
+     */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/api/get/{id}")
     public String get(int id) {
+        
         //Trace
-        registry.counter("api liste", Tags.of("name", "get")).increment();
+        registry.counter("api get", Tags.of("name", "get = "+id)).increment();
 
         return personneService.toJSON(personneService.get(id)).toString();
     }
 
+    /*
+     * add
+     */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/api/add")
     public String add() {
         
+         registry.counter("api add", Tags.of("name", "add ")).increment();
+ 
         personneService.add(new Personne("toto","toto","16"));
         
         JSONObject obj = new JSONObject();
         obj.put("add", "ajoute avec succes");
+        return obj.toString();
+    }
+
+    /*
+     * add
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/api/version")
+    public String version() {
+        
+        JSONObject obj = new JSONObject();
+        obj.put("version", applicationVersion);
         return obj.toString();
     }
 
