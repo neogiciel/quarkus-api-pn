a79337181fefe500d49329ee068f36a4db077a0c
neogiciel <123723616+neogiciel@users.noreply.github.com>
2024-01-09 21:06:50 +0100

    sources v1.29

--- ci/mvn-script.sh
+++ ci/mvn-script.sh
@@ -8,4 +8,5 @@ mvn -f quarkus-api-pn/pom.xml clean package
 
 #docker
 echo "--------------------- cp --------------------"
-cp quarkus-api-pn/target/quarkus-api-pn-1.0.0-SNAPSHOT.jar  mvn-output
+#cp quarkus-api-pn/target/quarkus-api-pn-1.0.0-SNAPSHOT.jar  mvn-output
+cp quarkus-api-pn  mvn-output
