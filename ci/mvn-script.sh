f7539c56048316af4f2a90d764973a038a946e29
neogiciel <123723616+neogiciel@users.noreply.github.com>
2024-01-09 20:54:18 +0100

    sources v1.28

--- ci/mvn-script.sh
+++ ci/mvn-script.sh
@@ -3,3 +3,6 @@ echo "starting test-script from directory: $ROOT"
 
 #cd _ROOT/AppDemo
 mvn -f quarkus-api-pn/pom.xml clean package
+
+#docker
+docker build -f quarkus-api-pn/src/main/docker/Dockerfile.jvm -t neogicel/quarkus-api-pn-jvm:1.0 .
